6fbba1512092b4dd00dec79793b45c777a70593e
Heybro-42 <adityagusti72@gmail.com>
2024-10-18 12:20:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ function selectDivision() {
     var selectedDivision = document.getElementById("divisionSelect").value;
     if (selectedDivision === "pengabdian") {
         document.getElementById("pengabdianList").classList.add('active');
+        launchConfetti();
     } else if (selectedDivision === "perhubungan") {
         document.getElementById("perhubunganList").classList.add('active');
+        launchConfetti();
     } else if (selectedDivision === "media") {
         document.getElementById("mediaList").classList.add('active');
+        launchConfetti();
     }
+}
+
+// Fungsi untuk memunculkan animasi konfeti
+function launchConfetti() {
+    var duration = 5 * 1000; // Durasi 5 detik
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
 }
